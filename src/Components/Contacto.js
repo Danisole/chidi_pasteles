9e86d3ba9a6eaafbcd22db5df4ed06cc219fe164
Danisole <danisole82@gmail.com>
2024-10-08 00:39:36 -0300

    alert

--- src/Components/Contacto.js
+++ src/Components/Contacto.js
@@ -1,14 +1,37 @@
 import React from "react";
+import Swal from "sweetalert2";
 
 const Contact = () => {
+  
+  const handleSubmit = (e) => {
+    e.preventDefault(); 
+
+    // Mostrar el SweetAlert
+    Swal.fire({
+      title: 'Consulta enviada!',
+      text: 'Nos pondremos en contacto contigo pronto.',
+      icon: 'success',
+      confirmButtonText: 'OK'
+    }).then(() => {
+      
+      document.getElementById("contactForm").submit(); 
+    });
+  };
+
   return (
     <div className="contact-page-wrapper" id="contacto">
       <p className="primary-subheading">Contacto</p>
       <h1 className="primary-heading">¿Tienes alguna pregunta?</h1>
       <h1 className="primary-heading">Te ayudamos</h1>
-      <form className="contact-form-container" action="https://formsubmit.co/danisole82@gmail.com" method="POST">
-        
-        <input type="text" placeholder="Envia tu consulta..." />
+
+      <form 
+        className="contact-form-container" 
+        action="https://formsubmit.co/danisole82@gmail.com" 
+        method="POST" 
+        id="contactForm"
+        onSubmit={handleSubmit}
+      >
+        <input type="text" placeholder="Envia tu consulta..." required />
         <button type="submit" className="secondary-button">Enviar</button>
 
         <input type="hidden" name="_next" value="http://localhost:3001/"/>
