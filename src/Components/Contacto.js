5ac4bf0f67975dc5906afcd12fb48b97607c281e
Danisole <danisole82@gmail.com>
2024-10-08 00:25:33 -0300

    form

--- src/Components/Contacto.js
+++ src/Components/Contacto.js
@@ -6,10 +6,14 @@ const Contact = () => {
       <p className="primary-subheading">Contacto</p>
       <h1 className="primary-heading">¿Tienes alguna pregunta?</h1>
       <h1 className="primary-heading">Te ayudamos</h1>
-      <div className="contact-form-container">
-        <input type="text" placeholder="chidimail@gmail.com" />
-        <button className="secondary-button">Enviar</button>
-      </div>
+      <form className="contact-form-container" action="https://formsubmit.co/danisole82@gmail.com" method="POST">
+        
+        <input type="text" placeholder="Envia tu consulta..." />
+        <button type="submit" className="secondary-button">Enviar</button>
+
+        <input type="hidden" name="_next" value="http://localhost:3001/"/>
+        <input type="hidden" name="_captcha" value="false" />
+      </form>
     </div>
   );
 };
